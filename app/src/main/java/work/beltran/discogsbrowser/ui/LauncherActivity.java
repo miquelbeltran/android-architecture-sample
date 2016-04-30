2ccb97ebe49755d8f96a512ce7ff1a78f9fddcb9
Miquel Beltran <mike513@gmail.com>
2016-04-30 08:35:45 +0200

    loading shared prefs

--- app/src/main/java/work/beltran/discogsbrowser/ui/LauncherActivity.java
+++ app/src/main/java/work/beltran/discogsbrowser/ui/LauncherActivity.java
@@ -2,6 +2,7 @@ package work.beltran.discogsbrowser.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,11 +12,18 @@ import work.beltran.discogsbrowser.ui.collection.CollectionActivity;
 public class LauncherActivity extends Activity {
 
     private static final String TAG = LauncherActivity.class.getCanonicalName();
+    private static final String PREFS_NAME = "DiscogsPreferences";
+    private static final String API_KEY = "ApiKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate()");
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        String apiKey = settings.getString(API_KEY, "");
+
+
         Intent intent = new Intent(this, CollectionActivity.class);
         startActivity(intent);
         finish();
