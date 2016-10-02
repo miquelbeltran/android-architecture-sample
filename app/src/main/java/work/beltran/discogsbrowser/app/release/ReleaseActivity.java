551c0b35c00c52eaa6ad153b966a5e077937cb43
Miquel Beltran <miquelbeltran@gmail.com>
2016-10-02 13:08:28 +0200

    Design for release activity

--- app/src/main/java/work/beltran/discogsbrowser/app/release/ReleaseActivity.java
+++ app/src/main/java/work/beltran/discogsbrowser/app/release/ReleaseActivity.java
@@ -3,8 +3,10 @@ package work.beltran.discogsbrowser.app.release;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 
 import work.beltran.discogsbrowser.R;
 
@@ -12,6 +14,7 @@ public class ReleaseActivity extends AppCompatActivity {
 
     private static final String EXTRA_RELEASE_ID = "EXTRA_RELEASE_ID";
     private static final String TAG = ReleaseActivity.class.getName();
+    ActionBar actionBar;
 
     public static Intent createReleaseActivity(Context context, int releaseId) {
         Intent intent = new Intent(context, ReleaseActivity.class);
@@ -25,5 +28,24 @@ public class ReleaseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_release);
         int releaseId = getIntent().getIntExtra(EXTRA_RELEASE_ID, 0);
         Log.d(TAG, "Release id: " + releaseId);
+        initActionBar();
     }
+
+    private void initActionBar() {
+        actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
