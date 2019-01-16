c356cc02ae6799dea533f762d046ded31759fd8b
Miquel Beltran <miquelbeltran@users.noreply.github.com>
2019-01-16 15:04:40 +0100

    Implemented unit test for GetAlbumDetailsUseCaseImpl

--- album-domain/src/test/java/work/beltran/discogsbrowser/album/domain/GetAlbumDetailsUseCaseImplTest.kt
+++ album-domain/src/test/java/work/beltran/discogsbrowser/album/domain/GetAlbumDetailsUseCaseImplTest.kt
@@ -1,7 +1,46 @@
 package work.beltran.discogsbrowser.album.domain
 
+import arrow.core.orNull
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
+import org.junit.Test
+import retrofit2.Response
+import work.beltran.discogsbrowser.api.DiscogsService
+import work.beltran.discogsbrowser.api.ReleaseDetails
 
 class GetAlbumDetailsUseCaseImplTest {
 
+    @Test
+    fun `get album details`() {
+        runBlocking {
+            val service = mockk<DiscogsService>()
+            val response = ReleaseDetails(
+                id = "id",
+                title = "title",
+                thumb = "thumb",
+                artists = listOf(
+                    work.beltran.discogsbrowser.common.domain.ArtistApi(
+                        id = "id",
+                        name = "name"
+                    )
+                )
+            )
+            every {
+                service.getRelease(releaseId = "id")
+            } returns CompletableDeferred(
+                Response.success(response)
+            )
+
+            val usecase = GetAlbumDetailsUseCaseImpl(service)
+            val result = usecase.getAlbum(id = "id").orNull()!!
+
+            assertEquals("id", result.id)
+            assertEquals("title", result.title)
+            assertEquals("thumb", result.thumb)
+            assertEquals("name", result.artist.head.name)
+        }
+    }
 }
